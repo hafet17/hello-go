5d89a97e6770956627d4c009570a6b64e5fd3ff8
hafet17 <do.crazy192@gmail.com>
2021-07-07 06:55:45 +0700

    add string

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Mohamad")
+	fmt.Println("Mohamad Hafid")
+	fmt.Println("Mohamad Hafid Masruri")
+
+	fmt.Println(len("Mohamad"))
+	fmt.Println("Mohamad Hafid"[0])
+	fmt.Println("Mohamad Hafid Masruri"[1])
+}
