f80e1fc891575c4615890ec9cd37111bdbc2d38c
hafet17 <do.crazy192@gmail.com>
2021-07-07 22:31:02 +0700

    add type-data-array

---
+++ typ-data-array.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var names [3]string
+	names[0] = "Fatih"
+	names[1] = "Nauval"
+	names[2] = "Azzidan"
+
+	fmt.Println(names[0])
+
+	var values = [3]int{
+		90,
+		88,
+		77,
+	}
+
+	values[0] = 99
+	fmt.Println(values)
+	fmt.Println("Panjang array names = ", len(names))
+	fmt.Println("Panjang array values = ", len(values))
+
+}
