14b8a750b31cbb4fd3cf2541595dcb719327d523
hafet17 <do.crazy192@gmail.com>
2021-07-18 21:23:06 +0700

    modif closure

--- 8.function/closure.go
+++ 8.function/closure.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	counter := 0
+	name := "Hafid"
+
+	increment := func() {
+		fmt.Println("increment")
+		counter++
+		name = "Budi"
+	}
+
+	increment()
+	fmt.Println(counter)
+	fmt.Println(name)
 
 }
