6ce017785822b66ec57561f34878226713487f0c
hafet17 <do.crazy192@gmail.com>
2021-07-18 21:23:35 +0700

    modif variadic_function

--- 8.function/variadic_function.go
+++ 8.function/variadic_function.go
@@ -10,9 +10,20 @@ func sumAll(numbers ...int) int {
 	return total
 }
 
+func sumKali(numbers ...int) int {
+	total := 1
+	for _, number := range numbers {
+		total *= number
+	}
+	return total
+}
+
 func main() {
 	total := sumAll(10, 20, 90, 80, 40)
 	fmt.Println(total)
+
+	kali := sumKali(5, 10, 2, 4, 2)
+	fmt.Println(kali)
 
 	slice := []int{10, 80, 70, 30, 20}
 	fmt.Println(sumAll(slice...))
